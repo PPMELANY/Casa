694ec6860e539e679048fee67b1358ed0dc97337
PPMELANY <melany.pintadop.est@uets.edu.ec>
2024-03-29 20:51:21 -0500

    Casa2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -24,8 +24,6 @@ export class HomePage {
     object(route).subscribe(attributes => {
       const valores_db = attributes.snapshot.val();
       console.log(valores_db);
-
-      // Obtener el estado de cada campo de la base de datos
       this.ledColorAtico = valores_db.atico;
       this.ledColorDormitorio = valores_db.dormitorio;
       this.ledColorCocina = valores_db.cocina;
@@ -38,91 +36,53 @@ export class HomePage {
   async dormitorio() {
     // Cambiar el estado del LED
     this.ledColor = !this.ledColor;
-
     // Cambiar el estado del dormitorio en la base de datos
     const routeDormitorio = ref(this.database, '/Casa/dormitorio');
     set(routeDormitorio, this.ledColor);
-
     // Actualizar el color del botón basado en el estado del dormitorio
     this.ledColorDormitorio = this.ledColor;
-
     // Agregar un mensaje de registro para mostrar el estado actual de encendido
     console.log('Estado del dormitorio:', this.ledColor);
   }
 
   async cocina() {
-    // Cambiar el estado del LED
     this.ledColor = !this.ledColor;
-
-    // Cambiar el estado de la cocina en la base de datos
     const routeCocina = ref(this.database, '/Casa/cocina');
     set(routeCocina, this.ledColor);
-
-    // Actualizar el color del botón basado en el estado de la cocina
     this.ledColorCocina = this.ledColor;
-
-    // Agregar un mensaje de registro para mostrar el estado actual de encendido
     console.log('Estado de la cocina:', this.ledColor);
   }
 
   async bamo() {
-    // Cambiar el estado del LED
     this.ledColor = !this.ledColor;
-
-    // Cambiar el estado del baño en la base de datos
     const routeBaño = ref(this.database, '/Casa/baño');
     set(routeBaño, this.ledColor);
-
-    // Actualizar el color del botón basado en el estado del baño
     this.ledColorBamo = this.ledColor;
-
-    // Agregar un mensaje de registro para mostrar el estado actual de encendido
     console.log('Estado del baño:', this.ledColor);
 }
 
 
   async sala() {
-    // Cambiar el estado del LED
     this.ledColor = !this.ledColor;
-
-    // Cambiar el estado de la sala en la base de datos
     const routeSala = ref(this.database, '/Casa/sala');
     set(routeSala, this.ledColor);
-
-    // Actualizar el color del botón basado en el estado de la sala
     this.ledColorSala = this.ledColor;
-
-    // Agregar un mensaje de registro para mostrar el estado actual de encendido
     console.log('Estado de la sala:', this.ledColor);
   }
 
   async atico() {
-    // Cambiar el estado del LED
     this.ledColor = !this.ledColor;
-
-    // Cambiar el estado de la sala en la base de datos
     const routeatico = ref(this.database, '/Casa/atico');
     set(routeatico, this.ledColor);
-
-    // Actualizar el color del botón basado en el estado de la sala
     this.ledColorAtico = this.ledColor;
-
-    // Agregar un mensaje de registro para mostrar el estado actual de encendido
     console.log('Estado de la sala:', this.ledColor);
   }
 
   async lavanderia() {
-    // Cambiar el estado del LED
     this.ledColor = !this.ledColor;
-
-    // Cambiar el estado de la sala en la base de datos
     const routelavanderia = ref(this.database, '/Casa/lavanderia');
     set(routelavanderia, this.ledColor);
-
-    // Actualizar el color del botón basado en el estado de la sala
     this.ledColorlavanderia = this.ledColor;
-
-    // Agregar un mensaje de registro para mostrar el estado actual de encendido
     console.log('Estado de la sala:', this.ledColor);
   }
 }
